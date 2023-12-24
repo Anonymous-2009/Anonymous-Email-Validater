6dfc4b7c479506ed9b6a488e30f8cd47076be8b3
Anonymous <krishnabag751@gmail.com>
2023-12-24 18:26:56 +0000

    new changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 let submit = document.getElementById('submit-button'); 
 
 let div = document.getElementByClassName('resultCont');
+
  submit.addEventListener("click", async (event) => {
     event.preventDefault();
     event.stopPropagation();
-    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    div.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
    let email = document.getElementById('email').value
    let url = `https://emailvalidation.abstractapi.com/v1/?api_key=60d04e1b23d64c9fabcbffdd4c03b84d&email=${email}`;
    let result = await fetch(url);
