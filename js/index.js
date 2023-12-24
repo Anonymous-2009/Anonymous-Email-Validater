0599de338927a86b62500de211b0f353760874e5
Anonymous <krishnabag751@gmail.com>
2023-12-24 18:25:19 +0000

    add new logic in script and better api

--- js/index.js
+++ js/index.js
@@ -1,22 +1,13 @@
+let submit = document.getElementById('submit-button'); 
 
-
-submitBtn.addEventListener("click", async (event) => {
+let div = document.getElementByClassName('resultCont');
+ submit.addEventListener("click", async (event) => {
     event.preventDefault();
     event.stopPropagation();
-    console.log("Clicked!")
     resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
-    let key = "60d04e1b23d64c9fabcbffdd4c03b84d"
-    let email = document.getElementById("username").value 
-    let url = `https://emailvalidation.abstractapi.com/v1/?api_key=${key}&email=${email}`
-    let res = await fetch(url)
-    let result = await res.json()
-    let str = ``
-    for (key of Object.keys(result)) {
-        if(result[key] !== "" && result[key]!== " "){ 
-            str = str + `<div>${key}: ${result[key]}</div>`
-        }
-    }
-
-    console.log(str)
-    resultCont.innerHTML = str
+   let email = document.getElementById('email').value
+   let url = `https://emailvalidation.abstractapi.com/v1/?api_key=60d04e1b23d64c9fabcbffdd4c03b84d&email=${email}`;
+   let result = await fetch(url);
+   let response = await result.json();
+   div.innerHTML =   response["deliverability"];
 })
